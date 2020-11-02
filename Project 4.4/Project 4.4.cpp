5cda5d859dffab4cdc4359698994e034cbdf8733
Orest Yakymiv <akimivorest95@gmail.com>
2020-11-02 16:28:38 +0200

    first commit

---
+++ Project 4.4/Project 4.4.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x, xp, xk, dx, R, y;
+
+    cout << "R = "; cin >> R;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+
+    cout << fixed;
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |";
+    cout << setw(10) << "y" << " |" << endl;
+    cout << "---------------------------" << endl;
+
+    x = xp;
+    while (x <= xk)
+    {
+
+        if (x <= -R)
+            y = R;
+            
+        else
+            if (-R < x && x <= R)
+                y = -sqrt(R * R - x * x) - R;
+            else
+                if (R < x && x <= 6)
+                    y = R + (-3 - R) * (x - 3) / (6 - R);
+                    else
+                    y = x - 9;
+
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << y
+            << " |" << endl;
+        x += dx;
+    }
+    cout << "---------------------------" << endl;
+
+    system("pause");
+    return 0;
+}
